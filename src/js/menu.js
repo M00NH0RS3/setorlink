9783bd0d3c654c063db44e5ca9202aec2c03e7a8
M00NH0RS3 <matheusmaciel.trampas@gmail.com>
2023-10-24 15:34:45 -0300

    m

---
+++ src/js/menu.js
@@ -0,0 +1,28 @@
+function acessarMenu() {
+    var btnLogin = document.getElementById('login');
+    
+    btnLogin.addEventListener('click', function() {
+        window.location.href = './src/templates/menu.html';
+    });
+}
+
+
+var menuItem = document.querySelectorAll('.item-menu')
+
+function selectLink(){
+    menuItem.forEach((item) =>
+        item.classList.remove('ativo')
+    )
+    this.classList.add('ativo')
+}
+
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
+)
+
+var btnExp = document.querySelector('#btn-exp')
+var menuSide = document.querySelector('.menu-lateral')
+
+btnExp.addEventListener('click', function(){
+    menuSide.classList.toggle('expandir')
+})
